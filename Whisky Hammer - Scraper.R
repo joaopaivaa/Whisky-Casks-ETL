37564cc117662c282d008a79ca8c2415766458b1
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2025-12-27 13:02:41 -0300

    started and finished Whisky Hammer scraper

---
+++ Whisky Hammer - Scraper.R
@@ -0,0 +1,49 @@
+# Loads the rvest, stringr, dplyr, glue and purrr libraries
+suppressWarnings({
+  suppressPackageStartupMessages({
+    library(rvest)
+    library(httr)
+    library(stringr)
+    library(dplyr)
+    library(glue)
+    library(purrr)
+    library(lubridate)
+  })
+})
+
+# Language: us-english
+Sys.setlocale("LC_ALL", "en_US.UTF-8")
+
+setwd("C:\\Users\\joaov\\Documents\\Whisky Casks ETL")
+
+search_page <- read_html("whiskyhammer_casks.html")
+
+lots_section <- search_page %>% html_nodes('div.itemsList') %>% html_nodes('div.item')
+
+lots_df <- data.frame()
+
+for (lot in lots_section) {
+  
+  lot_title <- lot %>% html_nodes('a.itemTitle') %>% html_text()
+  
+  lot_url <- lot %>% html_nodes('a.itemTitle') %>% html_attr('href')
+  
+  lot_auction_date <- lot %>% html_nodes('div.itemPriceWrap span.current') %>% html_text() %>% str_replace('Sold', '') %>% trimws()
+  
+  lot_price <- lot %>% html_nodes('div.itemPriceWrap span.priceStandard span.multiprice span.GBP.show') %>% html_text() %>% str_replace('£', '') %>% str_replace(',', '') %>% trimws()
+  
+  lot_image <- lot %>% html_nodes('div.itemImageWrap a img.js_lazy.entered.loaded') %>% html_attr('src') %>% str_replace('./whiskyhammer_casks_files/', '') %>% paste0('https://www.whiskyhammer.com/uploads/images/products/newthumbs/', .)
+  
+  lot_df <- data.frame(lot_title = lot_title,
+                       lot_url = lot_url,
+                       lot_auction_date = lot_auction_date,
+                       lot_price = lot_price,
+                       lot_image = lot_image)
+  
+  lots_df <- bind_rows(lots_df, lot_df)
+  
+}
+
+setwd("C:\\Users\\joaov\\Documents\\Whisky Casks ETL\\bronze")
+
+write.csv2(lots_df, 'Whisky Hammer - Casks Database.csv')
